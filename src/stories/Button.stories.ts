3194bcb4f72d27ce0193597a8fd4116ad1ad3ac0
Hitesh Shimpi <hiteshshimpi@Hiteshs-MacBook-Air.local>
2024-12-14 20:41:52 +0530

    New initialization

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,4 +1,4 @@
-import { Button } from '@/components/ui/button'
+import { Button } from '@/component/button';
 import type { Meta, StoryObj } from '@storybook/react'
 import { fn } from '@storybook/test'
 
