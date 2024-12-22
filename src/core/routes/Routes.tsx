5614b7a7cd0c0e8f43fd50193126cef7f68fe0b8
omkar273 <omkarsonawane622@gmail.com>
2024-12-22 23:37:33 +0530

    feat(sidebar): add : sidebar , sidebar components

--- src/core/routes/Routes.tsx
+++ src/core/routes/Routes.tsx
@@ -1,26 +1,25 @@
-import MainLayout from "@/layouts/MainLayout";
-import Auth from "@/pages/auth/Auth";
-import { createBrowserRouter } from "react-router-dom";
-import CustomerPage from "@/pages/customer/Customer";
-import AuthMiddleware from "../auth/AuthProvider";
-
+import MainLayout from '@/layouts/MainLayout';
+import Auth from '@/pages/auth/Auth';
+import { createBrowserRouter } from 'react-router-dom';
+import CustomerPage from '@/pages/customer/Customer';
+import AuthMiddleware from '../auth/AuthProvider';
 
 const RouteNames = {
-    home:{
-        path: '/'
-    },
-    login:{
-        path: '/login'
-    },
+	home: {
+		path: '/',
+	},
+	login: {
+		path: '/login',
+	},
 	plans: {
 		path: '/plans',
-        create: {
-            path: '/create',
-        },
-        edit: {
-            path: '/edit/:id',
-            routing_path: '/edit/'
-        }
+		create: {
+			path: '/create',
+		},
+		edit: {
+			path: '/edit/:id',
+			routing_path: '/edit/',
+		},
 	},
 	metering: {
 		path: '/metering',
@@ -30,31 +29,35 @@ const RouteNames = {
 	},
 	customer: {
 		path: '/customer',
-        detail:{
-            path: '/details/:id',
-            routing_path: '/details/'
-        }
-	}
+		detail: {
+			path: '/details/:id',
+			routing_path: '/details/',
+		},
+	},
 };
 
 export const MainRouter = createBrowserRouter([
-    {
-        path: RouteNames.login.path,
-        element: <Auth/>
-    },
+	{
+		path: RouteNames.login.path,
+		element: <Auth />,
+	},
 
-    {
-        path: RouteNames.home.path,
-        element: <AuthMiddleware requiredRole={['admin']}><MainLayout/></AuthMiddleware>,
-        children:[
-            {
-                path: '/',
-                element: <CustomerPage />,
-            },
-            {
-                path: RouteNames.plans.path,
-                element: <CustomerPage/>
-            }
-        ]
-    }
-])
+	{
+		path: RouteNames.home.path,
+		element: (
+			<AuthMiddleware requiredRole={['admin']}>
+				<MainLayout />
+			</AuthMiddleware>
+		),
+		children: [
+			{
+				path: '/',
+				element: <CustomerPage />,
+			},
+			{
+				path: RouteNames.plans.path,
+				element: <CustomerPage />,
+			},
+		],
+	},
+]);
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,41 +1,49 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import Sidebar from '@/components/molecules/Sidebar';
 
 const MainLayout: React.FC = () => {
-  const location = useLocation();
+	const location = useLocation();
 
-  const pathSegments = location.pathname.split('/').filter((segment) => segment);
+	const pathSegments = location.pathname.split('/').filter((segment) => segment);
 
-  return (
-    <div className="flex h-screen bg-gray-100">
-      <div className="flex flex-col flex-1">
-        <header className="bg-white shadow-sm">
-          <div className="px-6 py-4">
-            <nav className="flex items-center space-x-2 text-sm text-gray-500">
-              {pathSegments.map((segment, index) => {
-                const path = `/${pathSegments.slice(0, index + 1).join('/')}`;
-                const name = decodeURIComponent(segment);
+	return (
+		<SidebarProvider className='flex h-screen bg-gray-100'>
+			{/* sidebar */}
+			<Sidebar />
 
-                return (
-                  <span key={index} className="flex items-center">
-                    <a href={path} className="hover:text-gray-800 capitalize">
-                      {name}
-                    </a>
-                    {index < pathSegments.length - 1 && <span className="mx-2">/</span>}
-                  </span>
-                );
-              })}
-            </nav>
-          </div>
-        </header>
+			{/* right layout  */}
+			<SidebarInset className='flex flex-col flex-1'>
+				<header className='bg-white shadow-sm'>
+					<div className='px-6 py-4'>
+						{/* breadcrumbs */}
+						<nav className='flex items-center space-x-2 text-sm text-gray-500'>
+							{pathSegments.map((segment, index) => {
+								const path = `/${pathSegments.slice(0, index + 1).join('/')}`;
+								const name = decodeURIComponent(segment);
 
-        <main className="flex-1 p-6">
-          <Outlet/>
-        </main>
-      </div>
-      <Toaster/>
-    </div>
-  );
+								return (
+									<span key={index} className='flex items-center'>
+										<a href={path} className='hover:text-gray-800 capitalize'>
+											{name}
+										</a>
+										{index < pathSegments.length - 1 && <span className='mx-2'>/</span>}
+									</span>
+								);
+							})}
+						</nav>
+					</div>
+				</header>
+
+				{/* main layout */}
+				<main className='flex-1 p-6'>
+					<Outlet />
+				</main>
+			</SidebarInset>
+			<Toaster />
+		</SidebarProvider>
+	);
 };
 
 export default MainLayout;
