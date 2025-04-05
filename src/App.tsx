7698ca7098d2abeb3c1db145eff8b15726555f28
omkar sonawane <omkarsonawane622@gmail.com>
2025-04-05 13:36:29 +0530

    refactor(App): integrate ReactQueryProvider into application structure
    - Wrapped existing providers with ReactQueryProvider to enhance data fetching capabilities and maintain consistent context management.

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,37 @@ import { MainRouter } from '@/core/routes/Routes';
 import { UserProvider } from '@/hooks/UserContext';
 import { Toaster } from 'react-hot-toast';
 import { DocsProvider } from './context/DocsContext';
+import ReactQueryProvider from './core/tanstack/ReactQueryProvider';
 // import VercelSpeedInsights from '@/core/vercel/vercel';
 
 const App = () => {
 	return (
-		<UserProvider>
-			<DocsProvider>
-				<RouterProvider router={MainRouter} />
-				{/* <VercelSpeedInsights /> */}
-			</DocsProvider>
-			{/* Toast Notifications */}
-			<Toaster
-				toastOptions={{
-					success: {
-						iconTheme: {
-							primary: '#5CA7A0',
-							secondary: '#fff',
+		<ReactQueryProvider>
+			<UserProvider>
+				<DocsProvider>
+					<RouterProvider router={MainRouter} />
+					{/* <VercelSpeedInsights /> */}
+				</DocsProvider>
+				{/* Toast Notifications */}
+				<Toaster
+					toastOptions={{
+						success: {
+							iconTheme: {
+								primary: '#5CA7A0',
+								secondary: '#fff',
+							},
 						},
-					},
-					error: {
-						iconTheme: {
-							primary: '#E76E50',
-							secondary: '#fff',
+						error: {
+							iconTheme: {
+								primary: '#E76E50',
+								secondary: '#fff',
+							},
 						},
-					},
-				}}
-				position='bottom-center'
-			/>
-		</UserProvider>
+					}}
+					position='bottom-center'
+				/>
+			</UserProvider>
+		</ReactQueryProvider>
 	);
 };
 
