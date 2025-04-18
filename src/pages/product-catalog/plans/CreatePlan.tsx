573a869622287a964ba4d724641c15414aaf58b7
omkar sonawane <omkarsonawane622@gmail.com>
2025-04-18 18:19:02 +0530

    feat(plan): simplify plan creation flow

--- src/pages/product-catalog/plans/CreatePlan.tsx
+++ src/pages/product-catalog/plans/CreatePlan.tsx
@@ -1,6 +1,5 @@
-import { Button, FormHeader, Spacer, Stepper } from '@/components/atoms';
-import { PlanDetailsSection, SetupChargesSection } from '@/components/organisms';
-import { ApiDocsContent } from '@/components/molecules';
+import { Button, FormHeader, Spacer } from '@/components/atoms';
+import { PlanDetailsSection } from '@/components/organisms';
 import { RouteNames } from '@/core/routes/Routes';
 import { refetchQueries } from '@/core/tanstack/ReactQueryProvider';
 import { Plan } from '@/models/Plan';
@@ -9,9 +8,8 @@ import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+
 const CreatePlanPage = () => {
-	const [activeStep, setActiveStep] = useState(0);
-	const formSteps = [{ label: 'Plan Details' }, { label: 'Set up Charges' }];
 	const navigate = useNavigate();
 
 	const [tempPlan, setTempPlan] = useState<Partial<Plan>>({
@@ -21,11 +19,11 @@ const CreatePlanPage = () => {
 		prices: [],
 	});
 
+	const [errors, setErrors] = useState<Partial<Record<keyof Plan, string>>>({});
+
 	const setPlanField = <K extends keyof Plan>(field: K, value: Plan[K]) => {
 		setTempPlan((prev) => ({ ...prev, [field]: value }));
 	};
-
-	const [errors, setErrors] = useState<Partial<Record<keyof Plan, string>>>({});
 
 	const { mutate: submitPlan, isPending } = useMutation({
 		mutationFn: async () => {
@@ -44,61 +42,27 @@ const CreatePlanPage = () => {
 		},
 	});
 
-	const handleNext = () => {
-		if (isPending) {
-			return;
+	const validatePlan = () => {
+		const newErrors: Partial<Record<keyof Plan, string>> = {};
+
+		if (!tempPlan.name?.trim()) {
+			newErrors.name = 'Plan name is required';
 		}
 
-		if (activeStep === formSteps.length - 1) {
-			if (!validateSteps()) {
-				return;
-			}
+		if (!tempPlan.lookup_key?.trim()) {
+			newErrors.lookup_key = 'Plan slug is required';
+		}
+		setErrors(newErrors);
+		return Object.keys(newErrors).length === 0;
+	};
+
+	const handleSubmit = () => {
+		if (isPending) return;
+		console.log('validatePlan', validatePlan());
+		if (validatePlan()) {
 			submitPlan();
-			return;
 		}
-		if (!validateSteps()) {
-			return;
-		}
-		setActiveStep((prev) => prev + 1);
-	};
-
-	const handleBack = () => {
-		if (activeStep === 0) {
-			return;
-		}
-		setActiveStep((prev) => prev - 1);
-	};
-
-	const validateSteps = () => {
-		setErrors({});
-		if (activeStep === 0) {
-			if (!tempPlan.name) {
-				setErrors({ name: 'Plan name is required' });
-				return false;
-			}
-			if (!tempPlan.lookup_key) {
-				setErrors({ lookup_key: 'Plan slug is required' });
-				return false;
-			}
-		}
-		// else if (activeStep === 1) {
-		// 	if (!tempPlan.invoice_cadence) {
-		// 		setErrors({ invoice_cadence: 'Billing Timing is required' });
-		// 		return false;
-		// 	}
-		// 	if (metaData?.isTrialPeriod && !tempPlan.trial_period) {
-		// 		setErrors({ trial_period: 'Trial period is required' });
-		// 		return false;
-		// 	}
-		// }
-		else if (activeStep === 1) {
-			if (!tempPlan.prices || tempPlan.prices.length === 0) {
-				setErrors({ prices: 'At least one price tier is required' });
-				return false;
-			}
-		}
-
-		return true;
+		console.log('errors', errors);
 	};
 
 	return (
@@ -108,41 +72,21 @@ const CreatePlanPage = () => {
 				subtitle={'A Plan defines the features your customers have access to, the pricing structure, and the billing cadence.'}
 				variant='form-title'
 			/>
-			<ApiDocsContent tags={['Plans']} />
 
 			<Spacer height={'16px'} />
 
 			<div className='w-2/3'>
-				<Stepper activeStep={activeStep} steps={formSteps} />
-
+				<PlanDetailsSection plan={tempPlan} setPlanField={setPlanField} errors={errors} />
 				<Spacer height={'16px'} />
-
-				{activeStep === 0 && <PlanDetailsSection plan={tempPlan} setPlanField={setPlanField} errors={errors} />}
-
-				{activeStep === 1 && (
-					// <BillingPrefferencesSection
-					// 	plan={tempPlan}
-					// 	setPlanField={setPlanField}
-					// 	errors={errors}
-					// />
-					<SetupChargesSection plan={tempPlan} setPlanField={setPlanField} />
-				)}
-
-				{activeStep === 2 && <SetupChargesSection plan={tempPlan} setPlanField={setPlanField} />}
-
-				<Spacer height={'16px'} />
-				<div>
-					{activeStep > 0 && (
-						<Button onClick={handleBack} variant='secondary' className='mr-4 text-zinc-900'>
-							Go Back
-						</Button>
-					)}
-					<Button onClick={handleNext} variant='default' className='mr-4'>
-						{formSteps.length - 1 === activeStep ? 'Save' : 'Next'}
-					</Button>
-				</div>
+				<Button
+					onClick={handleSubmit}
+					variant='default'
+					className='mr-4'
+					disabled={isPending || !tempPlan.name || !tempPlan.lookup_key}
+					isLoading={isPending}>
+					Save
+				</Button>
 			</div>
-			{/* <pre>{JSON.stringify(tempPlan, null, 2)}</pre> */}
 		</div>
 	);
 };
