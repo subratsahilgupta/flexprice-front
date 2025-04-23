81cb642a36f7272dca09b91280447ec0efa7c9b9
omkar sonawane <omkarsonawane622@gmail.com>
2025-04-23 20:55:51 +0530

    fix(toolbar): make input sleek

--- src/pages/customer/customers/Customers.tsx
+++ src/pages/customer/customers/Customers.tsx
@@ -1,5 +1,5 @@
-import { AddButton, Loader, Page, ShortPagination, Spacer } from '@/components/atoms';
-import { CreateCustomerDrawer, ApiDocsContent, Toolbar, FilterState } from '@/components/molecules';
+import { AddButton, Input, Loader, Page, ShortPagination, Spacer } from '@/components/atoms';
+import { CreateCustomerDrawer, ApiDocsContent, FilterState } from '@/components/molecules';
 import CustomerTable from '@/components/molecules/Customer/CustomerTable';
 import EmptyPage from '@/components/organisms/EmptyPage/EmptyPage';
 import GUIDES from '@/core/constants/guides';
@@ -7,6 +7,7 @@ import usePagination from '@/hooks/usePagination';
 import Customer from '@/models/Customer';
 import CustomerApi from '@/utils/api_requests/CustomerApi';
 import { useQuery } from '@tanstack/react-query';
+import { Search } from 'lucide-react';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
 
@@ -61,29 +62,39 @@ const CustomerPage = () => {
 		<Page
 			heading='Customers'
 			headingCTA={
-				<CreateCustomerDrawer
-					trigger={
-						<AddButton
-							onClick={() => {
-								setactiveCustomer(undefined);
-							}}
-						/>
-					}
-					open={customerDrawerOpen}
-					onOpenChange={setcustomerDrawerOpen}
-					data={activeCustomer}
-				/>
+				<div className='flex justify-between items-center gap-2 items-center'>
+					<Input
+						className='min-w-[400px]'
+						suffix={<Search className='size-[14px] text-gray-500' />}
+						placeholder='Search by Name or lookup key'
+						value={filters.searchQuery}
+						onChange={(e) => setfilters({ ...filters, searchQuery: e })}
+						size='sm'
+					/>
+					<CreateCustomerDrawer
+						trigger={
+							<AddButton
+								onClick={() => {
+									setactiveCustomer(undefined);
+								}}
+							/>
+						}
+						open={customerDrawerOpen}
+						onOpenChange={setcustomerDrawerOpen}
+						data={activeCustomer}
+					/>
+				</div>
 			}>
 			<ApiDocsContent tags={['Customers']} />
 			<div>
-				<Toolbar
+				{/* <Toolbar
 					config={{
 						searchPlaceholder: 'Search by Name or lookup key',
 						enableSearch: true,
 					}}
 					filters={filters}
 					onFilterChange={(filterState) => setfilters(filterState as FilterState)}
-				/>
+				/> */}
 				{/* Conditional rendering for table or empty search state */}
 				{filters.searchQuery && isFetching ? (
 					<div className='flex justify-center py-4'>
