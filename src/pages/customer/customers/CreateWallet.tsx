45c8c4e61dd3c763dde86e4bcad623a6c6ff988f
omkar sonawane <omkarsonawane622@gmail.com>
2025-06-28 17:02:58 +0530

    feat(wallet): move create wallet to topup

--- src/pages/customer/customers/CreateWallet.tsx
+++ src/pages/customer/customers/CreateWallet.tsx
@@ -1,4 +1,4 @@
-import { Button, DatePicker, FormHeader, Input, Select, Spacer } from '@/components/atoms';
+import { Button, DatePicker, Input, Select } from '@/components/atoms';
 import { currencyOptions } from '@/constants/constants';
 import { refetchQueries } from '@/core/services/tanstack/ReactQueryProvider';
 import { cn } from '@/lib/utils';
@@ -9,13 +9,16 @@ import { useMutation } from '@tanstack/react-query';
 import { FC, useState } from 'react';
 import toast from 'react-hot-toast';
 import { CreateWalletPayload } from '@/types/dto';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 
 interface Props {
 	customerId: string;
 	onSuccess?: (walletId: string) => void;
+	open: boolean;
+	onOpenChange: (open: boolean) => void;
 }
 
-const CreateWallet: FC<Props> = ({ customerId, onSuccess = () => {} }) => {
+const CreateWallet: FC<Props> = ({ customerId, onSuccess = () => {}, open, onOpenChange }) => {
 	const [errors, setErrors] = useState({
 		currency: '',
 		name: '',
@@ -72,125 +75,77 @@ const CreateWallet: FC<Props> = ({ customerId, onSuccess = () => {} }) => {
 	};
 
 	return (
-		<div>
-			<FormHeader
-				title='Create Wallet'
-				subtitle={`Manage credits for usage-based billing that can apply to invoices pre-tax.`}
-				variant='form-title'
-			/>
-			<Spacer className='!mt-4' />
-			<div className='card space-y-4'>
-				<FormHeader title='Wallet setup' subtitle={`Define the wallet details and the currency it will operate in.`} variant='sub-header' />
-				<Input
-					error={errors.name}
-					value={walletPayload.name}
-					onChange={(e) => setwalletPayload({ ...walletPayload, name: e })}
-					label='Wallet Name'
-					placeholder='Enter wallet name'
-				/>
+		<Dialog open={open} onOpenChange={onOpenChange}>
+			<DialogContent className='bg-white sm:max-w-[600px] max-h-[80vh] overflow-y-auto'>
+				<DialogHeader>
+					<DialogTitle>Create Wallet</DialogTitle>
+					<DialogDescription>Define the wallet details and the currency it will operate in.</DialogDescription>
+				</DialogHeader>
+				<div className='grid gap-4 py-4'>
+					<Input
+						error={errors.name}
+						value={walletPayload.name}
+						onChange={(e) => setwalletPayload({ ...walletPayload, name: e })}
+						label='Wallet Name'
+						placeholder='Enter wallet name'
+					/>
 
-				<Select
-					value={walletPayload.currency}
-					options={currencyOptions}
-					label='Select Currency'
-					onChange={(e) => setwalletPayload({ ...walletPayload, currency: e })}
-					placeholder='Select Currency'
-					error={errors.currency}
-				/>
+					<Select
+						value={walletPayload.currency}
+						options={currencyOptions}
+						label='Select Currency'
+						onChange={(e) => setwalletPayload({ ...walletPayload, currency: e })}
+						placeholder='Select Currency'
+						error={errors.currency}
+					/>
 
-				<div className='flex flex-col items-start gap-2 w-full'>
-					<label className={cn('font-inter block text-sm font-medium', 'text-zinc-950')}>Conversion Rate</label>
-					<div className='flex items-center gap-2 w-full'>
-						<Input className='w-full' value={'1'} disabled suffix='credit' />
-						<span>=</span>
-						<Input
-							className='w-full'
-							variant='number'
-							suffix={getCurrencySymbol(walletPayload.currency || '')}
-							value={walletPayload.conversion_rate}
-							onChange={(e) => {
-								setwalletPayload({ ...walletPayload, conversion_rate: e as unknown as number });
+					<div className='flex flex-col items-start gap-2 w-full'>
+						<label className={cn('font-inter block text-sm font-medium', 'text-zinc-950')}>Conversion Rate</label>
+						<div className='flex items-center gap-2 w-full'>
+							<Input className='w-full' value={'1'} disabled suffix='credit' />
+							<span>=</span>
+							<Input
+								className='w-full'
+								variant='number'
+								suffix={getCurrencySymbol(walletPayload.currency || '')}
+								value={walletPayload.conversion_rate}
+								onChange={(e) => {
+									setwalletPayload({ ...walletPayload, conversion_rate: e as unknown as number });
+								}}
+							/>
+						</div>
+					</div>
+					<Input
+						label='Free Credits'
+						suffix='credits'
+						variant='formatted-number'
+						placeholder='Enter Free Credits to be added to the wallet'
+						value={walletPayload.initial_credits_to_load}
+						onChange={(e) => {
+							setwalletPayload({ ...walletPayload, initial_credits_to_load: e as unknown as number });
+						}}
+					/>
+					<div>
+						<DatePicker
+							labelClassName='text-foreground'
+							label='Free Credits Expiry Date'
+							minDate={new Date()}
+							placeholder='Select Expiry Date'
+							date={walletPayload.initial_credits_expiry_date_utc}
+							setDate={(e) => {
+								setwalletPayload({ ...walletPayload, initial_credits_expiry_date_utc: e as unknown as Date });
 							}}
 						/>
 					</div>
-				</div>
-				<Input
-					label='Free Credits'
-					suffix='credits'
-					variant='formatted-number'
-					placeholder='Enter Free Credits to be added to the wallet'
-					value={walletPayload.initial_credits_to_load}
-					onChange={(e) => {
-						setwalletPayload({ ...walletPayload, initial_credits_to_load: e as unknown as number });
-					}}
-				/>
-				<DatePicker
-					labelClassName='text-foreground'
-					label='Free Credits Expiry Date'
-					minDate={new Date()}
-					placeholder='Select Expiry Date'
-					date={walletPayload.initial_credits_expiry_date_utc}
-					setDate={(e) => {
-						setwalletPayload({ ...walletPayload, initial_credits_expiry_date_utc: e as unknown as Date });
-					}}
-				/>
-			</div>
-			{/* <Spacer className='!mt-4' />
-			<PremiumFeature isPremiumFeature>
-				<div className='card relative'>
-					<span className='absolute top-3 right-3'>
-						<PremiumFeatureTag />
-					</span>
-					<FormHeader
-						title='Automatic Wallet Top Up'
-						subtitle={`Never run out of balance. Set up automatic top-ups to stay worry-free.`}
-						variant='sub-header'
-					/>
-					<div className='flex items-center space-x-4 font-open-sans'>
-						<Switch
-							id='airplane-mode'
-							checked={autoTopup}
-							onCheckedChange={(value) => {
-								setautoTopup(value);
-							}}
-						/>
-						<Label htmlFor='airplane-mode'>
-							<p className='font-medium text-sm text-[#18181B] peer-checked:text-black'>Recharge Wallet Automatically</p>
-						</Label>
+
+					<div className='w-full justify-end flex'>
+						<Button isLoading={isPending} disabled={isPending} onClick={handleCreateWallet}>
+							Save Wallet
+						</Button>
 					</div>
 				</div>
-			</PremiumFeature> */}
-
-			{/* {autoTopup && (
-				<div className='space-y-4 mt-4'>
-					<Input
-						variant='number'
-						suffix='credits'
-						inputPrefix={walletPayload.currency ? getCurrencySymbol(walletPayload.currency) : undefined}
-						label='Enter minimum balance amount below which we top up '
-						placeholder='Enter Minimum Balance'
-					/>
-					<Input
-						variant='number'
-						suffix='credits'
-						inputPrefix={walletPayload.currency ? getCurrencySymbol(walletPayload.currency) : undefined}
-						label='How much should we add?'
-						placeholder='Enter Topup Amount'
-						className='w-1/2'
-					/>
-					<div className='flex items-center space-x-2'>
-						{['+100', '+500', '+1000', '+2000'].map((item) => (
-							<button className='text-xs font-medium text-zinc-600 rounded-md px-2 py-[2px] bg-zinc-100'>{item}</button>
-						))}
-					</div>
-				</div>
-			)} */}
-
-			{/* save wallet */}
-			<Button className='!mt-4' disabled={isPending} onClick={handleCreateWallet}>
-				Save Wallet
-			</Button>
-		</div>
+			</DialogContent>
+		</Dialog>
 	);
 };
 
--- src/pages/customer/tabs/Wallet.tsx
+++ src/pages/customer/tabs/Wallet.tsx
@@ -42,7 +42,7 @@ const WalletTab = () => {
 		setQueryParam,
 	} = useQueryParams<{ activeWalletId?: string }>({ activeWalletId: '' });
 
-	const [isAdd, setIsAdd] = useState(false);
+	const [showCreateWalletModal, setShowCreateWalletModal] = useState(false);
 	const [showTopupModal, setShowTopupModal] = useState(false);
 	const [showTerminateModal, setShowTerminateModal] = useState(false);
 	const [activeWallet, setActiveWallet] = useState<Wallet | null>();
@@ -96,20 +96,24 @@ const WalletTab = () => {
 			{
 				icon: <WalletIcon />,
 				label: 'Create Wallet',
-				onSelect: () => setIsAdd(true),
+				onSelect: () => setShowCreateWalletModal(true),
 			},
-			{
-				icon: <Pencil />,
-				label: 'Edit',
-				disabled: true,
-			},
-			{
-				icon: <Trash2 />,
-				label: 'Terminate',
-				onSelect: () => setShowTerminateModal(true),
-			},
+			...(activeWallet
+				? [
+						{
+							icon: <Pencil />,
+							label: 'Edit',
+							disabled: true,
+						},
+						{
+							icon: <Trash2 />,
+							label: 'Terminate',
+							onSelect: () => setShowTerminateModal(true),
+						},
+					]
+				: []),
 		],
-		[],
+		[activeWallet],
 	);
 
 	// Effect to set initial active wallet
@@ -138,34 +142,22 @@ const WalletTab = () => {
 		toast.error('An error occurred while fetching wallet details');
 	}
 
-	// Render create wallet if no wallets exist
-	if (isAdd) {
-		return (
-			<CreateWallet
-				onSuccess={(walletId) => {
-					setIsAdd(false);
-					setActiveWallet(wallets?.find((wallet) => wallet.id === walletId) || null);
-					setQueryParam('activeWalletId', walletId);
-				}}
-				customerId={customerId!}
-			/>
-		);
-	}
-
-	if (!wallets?.length) {
-		return (
-			<NoDataCard
-				title='Wallets'
-				subtitle='No wallets linked to the customer'
-				cta={!isArchived && <AddButton onClick={() => setIsAdd(true)} />}
-			/>
-		);
-	}
-
 	// Render wallet details
 	return (
 		<div className='space-y-6'>
 			<ApiDocsContent tags={['Wallets', 'Topup']} />
+
+			{/* Create Wallet Modal */}
+			<CreateWallet
+				customerId={customerId!}
+				open={showCreateWalletModal}
+				onOpenChange={setShowCreateWalletModal}
+				onSuccess={(walletId) => {
+					setShowCreateWalletModal(false);
+					setActiveWallet(wallets?.find((wallet) => wallet.id === walletId) || null);
+					setQueryParam('activeWalletId', walletId);
+				}}
+			/>
 
 			{/* Topup Modal */}
 			<Dialog open={showTopupModal} onOpenChange={() => setShowTopupModal(false)}>
@@ -184,143 +176,157 @@ const WalletTab = () => {
 				<TerminateWalletModal isOpen={showTerminateModal} onOpenChange={() => setShowTerminateModal(false)} wallet={activeWallet} />
 			)}
 
-			<FormHeader
-				className='!my-6'
-				title='Wallets'
-				subtitle='Manage credits for usage-based billing that can apply to invoices pre-tax'
-				variant='sub-header'
-			/>
+			{!wallets?.length ? (
+				<NoDataCard
+					title='Wallets'
+					subtitle='No wallets linked to the customer'
+					cta={!isArchived && <AddButton onClick={() => setShowCreateWalletModal(true)} />}
+				/>
+			) : (
+				<>
+					<FormHeader
+						className='!my-6'
+						title='Wallets'
+						subtitle='Manage credits for usage-based billing that can apply to invoices pre-tax'
+						variant='sub-header'
+					/>
 
-			{/* Wallet Selection and Actions */}
-			<div className='w-full flex justify-between items-center mb-3'>
-				<div>
-					{walletOptions.length > 1 && (
-						<div className='min-w-[250px]'>
-							<Select
-								options={walletOptions}
-								value={activeWallet?.id}
-								onChange={(value) => {
-									const selectedWallet = wallets?.find((wallet) => wallet.id === value) || null;
-									setActiveWallet(selectedWallet);
-									setQueryParam('activeWalletId', value);
-								}}
+					{/* Wallet Selection and Actions */}
+					<div className='w-full flex justify-between items-center mb-3'>
+						<div>
+							{walletOptions.length > 1 && (
+								<div className='min-w-[250px]'>
+									<Select
+										options={walletOptions}
+										value={activeWallet?.id}
+										onChange={(value) => {
+											const selectedWallet = wallets?.find((wallet) => wallet.id === value) || null;
+											setActiveWallet(selectedWallet);
+											setQueryParam('activeWalletId', value);
+										}}
+									/>
+								</div>
+							)}
+						</div>
+						<div className='flex items-center space-x-2'>
+							{!isArchived && (
+								<>
+									{activeWallet && (
+										<Button onClick={() => setShowTopupModal(true)}>
+											<WalletIcon />
+											<span>Topup Wallet</span>
+										</Button>
+									)}
+								</>
+							)}
+
+							<DropdownMenu
+								options={dropdownOptions}
+								trigger={<Button variant={'outline'} prefixIcon={<EllipsisVertical />} size={'icon'}></Button>}
 							/>
 						</div>
-					)}
-				</div>
-				<div className='flex items-center space-x-2'>
-					{!isArchived && (
-						<Button onClick={() => setShowTopupModal(true)}>
-							<WalletIcon />
-							<span>Topup Wallet</span>
-						</Button>
-					)}
+					</div>
 
-					<DropdownMenu
-						options={dropdownOptions}
-						trigger={<Button variant={'outline'} prefixIcon={<EllipsisVertical />} size={'icon'}></Button>}
-					/>
-				</div>
-			</div>
+					{/* Active Wallet Details */}
+					{activeWallet && (
+						<div>
+							<DetailsCard
+								variant='stacked'
+								title='Wallet Details'
+								data={[
+									{ label: 'Wallet Name', value: activeWallet?.name || 'Prepaid wallet' },
+									{
+										label: 'Status',
+										value: (
+											<Chip
+												variant={formatWalletStatus(activeWallet?.wallet_status) === 'Active' ? 'success' : 'default'}
+												label={formatWalletStatus(activeWallet?.wallet_status)}
+											/>
+										),
+									},
+									{
+										label: 'Conversion Rate',
+										value: <span>{`1 Credit = ${activeWallet?.conversion_rate}${getCurrencySymbol(activeWallet?.currency ?? '')}`}</span>,
+									},
+								]}
+							/>
+							<Spacer className='!h-4' />
 
-			{/* Active Wallet Details */}
-			{activeWallet && !isAdd && (
-				<div>
-					<DetailsCard
-						variant='stacked'
-						title='Wallet Details'
-						data={[
-							{ label: 'Wallet Name', value: activeWallet?.name || 'Prepaid wallet' },
-							{
-								label: 'Status',
-								value: (
-									<Chip
-										variant={formatWalletStatus(activeWallet?.wallet_status) === 'Active' ? 'success' : 'default'}
-										label={formatWalletStatus(activeWallet?.wallet_status)}
-									/>
-								),
-							},
-							{
-								label: 'Conversion Rate',
-								value: <span>{`1 Credit = ${activeWallet?.conversion_rate}${getCurrencySymbol(activeWallet?.currency ?? '')}`}</span>,
-							},
-						]}
-					/>
-					<Spacer className='!h-4' />
+							{/* Wallet Balance */}
+							{isBalanceLoading ? (
+								<Skeleton className='w-full h-[200px]' />
+							) : (
+								<div className='w-full grid grid-cols-2 gap-4'>
+									{['Current', 'Ongoing'].map((type, index) => (
+										<Card key={index}>
+											<div className='flex justify-between items-center mb-4'>
+												<div className='flex items-center space-x-2'>
+													<span className='text-gray-600 text-sm font-medium'>{type} Balance</span>
+													<TooltipProvider delayDuration={0}>
+														<Tooltip>
+															<TooltipTrigger>
+																<Info className='size-4 text-gray-400 hover:text-gray-600 transition-colors' />
+															</TooltipTrigger>
+															<TooltipContent>
+																<p>{type === 'Current' ? 'Balance as per latest invoice' : 'Includes real-time usage'}</p>
+															</TooltipContent>
+														</Tooltip>
+													</TooltipProvider>
+												</div>
+												<div className='opacity-50 group-hover:opacity-100 transition-opacity'>
+													<WalletIcon className='size-5 text-gray-500' />
+												</div>
+											</div>
 
-					{/* Wallet Balance */}
-					{isBalanceLoading ? (
-						<Skeleton className='w-full h-[200px]' />
-					) : (
-						<div className='w-full grid grid-cols-2 gap-4'>
-							{['Current', 'Ongoing'].map((type, index) => (
-								<Card key={index}>
-									<div className='flex justify-between items-center mb-4'>
-										<div className='flex items-center space-x-2'>
-											<span className='text-gray-600 text-sm font-medium'>{type} Balance</span>
-											<TooltipProvider delayDuration={0}>
-												<Tooltip>
-													<TooltipTrigger>
-														<Info className='size-4 text-gray-400 hover:text-gray-600 transition-colors' />
-													</TooltipTrigger>
-													<TooltipContent>
-														<p>{type === 'Current' ? 'Balance as per latest invoice' : 'Includes real-time usage'}</p>
-													</TooltipContent>
-												</Tooltip>
-											</TooltipProvider>
-										</div>
-										<div className='opacity-50 group-hover:opacity-100 transition-opacity'>
-											<WalletIcon className='size-5 text-gray-500' />
-										</div>
-									</div>
+											<div className='flex items-baseline space-x-2'>
+												<span className='text-gray-500 text-2xl font-medium'>{getCurrencySymbol(walletBalance?.currency ?? '')}</span>
+												<span className='text-5xl font-bold text-gray-900 leading-tight'>
+													{type === 'Current'
+														? formatAmount(walletBalance?.balance.toString() ?? '0')
+														: formatAmount(walletBalance?.real_time_balance.toString() ?? '0')}
+												</span>
+											</div>
 
-									<div className='flex items-baseline space-x-2'>
-										<span className='text-gray-500 text-2xl font-medium'>{getCurrencySymbol(walletBalance?.currency ?? '')}</span>
-										<span className='text-5xl font-bold text-gray-900 leading-tight'>
-											{type === 'Current'
-												? formatAmount(walletBalance?.balance.toString() ?? '0')
-												: formatAmount(walletBalance?.real_time_balance.toString() ?? '0')}
-										</span>
-									</div>
-
-									<div className='flex justify-between items-center'>
-										<span className='text-sm text-gray-500'>
-											{type === 'Current'
-												? formatAmount(walletBalance?.credit_balance.toString() ?? '0')
-												: formatAmount(walletBalance?.real_time_credit_balance.toString() ?? '0')}
-											{'  credits'}
-										</span>
-									</div>
-								</Card>
-							))}
-						</div>
-					)}
-					<Spacer className='!h-4' />
-
-					{/* Transactions */}
-					{transactionsData?.items.length === 0 ? (
-						<div className='card'>
-							<FormHeader title='No transactions found' variant='sub-header' subtitle='No recent transactions' />
-						</div>
-					) : (
-						<div className='card'>
-							<div className='w-full flex justify-between items-center'>
-								<FormHeader title='Transactions' titleClassName='!font-semibold' variant='form-title' />
-								<div className='flex items-center space-x-2'>
-									<button className='px-2 py-1'>
-										<IoSearch className='size-4 text-[#09090B]' />
-									</button>
-									<button className='px-2 py-1'>
-										<SlidersHorizontal className='size-4 text-[#09090B]' />
-									</button>
+											<div className='flex justify-between items-center'>
+												<span className='text-sm text-gray-500'>
+													{type === 'Current'
+														? formatAmount(walletBalance?.credit_balance.toString() ?? '0')
+														: formatAmount(walletBalance?.real_time_credit_balance.toString() ?? '0')}
+													{'  credits'}
+												</span>
+											</div>
+										</Card>
+									))}
 								</div>
-							</div>
-							<Spacer className='!h-6' />
-							<WalletTransactionsTable data={transactionsData?.items || []} currency={walletBalance?.currency ?? ''} />
-							<ShortPagination unit='Transactions' totalItems={transactionsData?.pagination.total ?? 0} />
+							)}
+							<Spacer className='!h-4' />
+
+							{/* Transactions */}
+							{transactionsData?.items.length === 0 ? (
+								<div className='card'>
+									<FormHeader title='No transactions found' variant='sub-header' subtitle='No recent transactions' />
+								</div>
+							) : (
+								<div className='card'>
+									<div className='w-full flex justify-between items-center'>
+										<FormHeader title='Transactions' titleClassName='!font-semibold' variant='form-title' />
+										<div className='flex items-center space-x-2'>
+											<button className='px-2 py-1'>
+												<IoSearch className='size-4 text-[#09090B]' />
+											</button>
+											<button className='px-2 py-1'>
+												<SlidersHorizontal className='size-4 text-[#09090B]' />
+											</button>
+										</div>
+									</div>
+									<Spacer className='!h-6' />
+									<WalletTransactionsTable data={transactionsData?.items || []} currency={walletBalance?.currency ?? ''} />
+									<ShortPagination unit='Transactions' totalItems={transactionsData?.pagination.total ?? 0} />
+								</div>
+							)}
 						</div>
 					)}
-				</div>
+				</>
 			)}
 		</div>
 	);
