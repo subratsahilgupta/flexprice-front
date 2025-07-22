4de2cdd83b36830b1bd6ea4cd09b131bdaae4727
omkar sonawane <omkarsonawane622@gmail.com>
2025-07-22 16:24:04 +0530

    feat(Guides): add image URLs to tutorials for enhanced visual guidance

--- src/constants/guides.tsx
+++ src/constants/guides.tsx
@@ -13,16 +13,19 @@ const GUIDES: Record<
 	features: {
 		tutorials: [
 			{
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180993/FEATURES1_veomrd.svg',
 				title: 'How to create a feature?',
 				description: 'Explore how features work in Flexprice.',
 				onClick: () => openGuide('https://docs.flexprice.io/docs/Product%20catalogue/Features/Creating%20a%20feature'),
 			},
 			{
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180989/Features2_fbd39s.svg',
 				title: 'How to link features to plans?',
 				description: 'Link features to plans in Flexprice.',
 				onClick: () => openGuide('https://docs.flexprice.io/docs/Product%20catalogue/Features/Link%20features%20to%20plans'),
 			},
 			{
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180994/FEATURES_3_drkhb7.svg',
 				title: 'How to clone open ai pricing?',
 				description: 'Clone open ai / cursor style pricing in Flexprice.',
 				onClick: () =>
@@ -35,16 +38,19 @@ const GUIDES: Record<
 	plans: {
 		tutorials: [
 			{
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180993/PLAN_1_j6tdqv.svg',
 				title: 'Explore how plans work in Flexprice.',
 				description: 'Explore how plans work in Flexprice.',
 				onClick: () => openGuide('https://docs.flexprice.io/docs/Product%20catalogue/Plans/Overview'),
 			},
 			{
 				title: 'How to create a plan',
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180993/PLAN_2_oxi9ld.svg',
 				description: 'Create a new plan in Flexprice.',
 				onClick: () => openGuide('https://docs.flexprice.io/docs/Product%20catalogue/Plans/Creating%20a%20plan'),
 			},
 			{
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180995/PLAN_3_lfh1mi.svg',
 				title: 'How to choose between advance and arrear billing?',
 				description: 'Understand billing models used in plans.',
 				onClick: () => openGuide('https://docs.flexprice.io/docs/Product%20catalogue/Plans/Charges%20in%20plans/Advancevsarrear'),
@@ -56,15 +62,18 @@ const GUIDES: Record<
 			{
 				title: 'How to create a customer',
 				description: 'Explore how customers work in Flexprice.',
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180990/Customer_1_kf0ena.svg',
 				onClick: () => openGuide('https://docs.flexprice.io/docs/Customers/Overview'),
 			},
 			{
 				title: 'How to archive a customer',
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180991/Customer_2_ifiaof.svg',
 				description: 'Create a new customer in Flexprice.',
 				onClick: () => openGuide('https://docs.flexprice.io/docs/Customers/Creating%20a%20customer'),
 			},
 			{
 				title: 'How to create a subscription',
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180987/Customer_3_triyiv.svg',
 				description: 'Create a new subscription in Flexprice.',
 				onClick: () => openGuide('https://docs.flexprice.io/docs/Customers/Creating%20a%20subscription'),
 			},
@@ -75,15 +84,18 @@ const GUIDES: Record<
 			{
 				title: 'How to create an invoice',
 				description: 'Explore how invoices work in Flexprice.',
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180993/Invoice_1_lh9ved.svg',
 				onClick: () => openGuide('https://docs.flexprice.io/api-reference/invoices/create-a-new-invoice#create-a-new-invoice'),
 			},
 			{
 				title: 'How to manage invoices',
 				description: 'Manage invoices in Flexprice.',
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180991/invoice_2_v8fa71.svg',
 				onClick: () => openGuide('https://docs.flexprice.io/docs/Invoices/ManagingInvoice'),
 			},
 			{
 				title: 'How to handle partial payments',
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180989/iNvoice_3_glq1xo.svg',
 				description: 'Handle partial payments in Flexprice.',
 				onClick: () => openGuide('https://docs.flexprice.io/docs/Invoices/partial_payments'),
 			},
@@ -131,16 +143,19 @@ const GUIDES: Record<
 		tutorials: [
 			{
 				title: 'Create a new credit note',
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180987/CN1_vkg2kh.svg',
 				description: 'Explore how credit notes work in Flexprice.',
 				onClick: () => openGuide('https://docs.flexprice.io/api-reference/credit-notes/create-a-new-credit-note'),
 			},
 			{
 				title: 'Process a draft credit note',
 				description: 'Create a new credit note in Flexprice.',
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180988/CN2_gpeaqi.svg',
 				onClick: () => openGuide('https://docs.flexprice.io/api-reference/credit-notes/process-a-draft-credit-note'),
 			},
 			{
 				title: 'Void a credit note',
+				imageUrl: 'https://res.cloudinary.com/daospvham/image/upload/v1753180987/CN3_kpsfpv.svg',
 				description: 'Manage credit notes in Flexprice.',
 				onClick: () => openGuide('https://docs.flexprice.io/api-reference/credit-notes/void-a-credit-note'),
 			},
